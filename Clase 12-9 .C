bfca35d7820d01dcd30eb60949e7b188f7529cfe
ytnaS <127530748+ytnaS@users.noreply.github.com>
2023-09-19 19:30:12 -0300

    Add files via upload

---
+++ Clase 12-9 .C
@@ -0,0 +1,31 @@
+/*
+Una matriz es un vectr con dos indices  0-1-2-3-4-5-6
+                                       0
+                                       1
+                                       2
+                                       3
+
+i j
+0 0
+1 1
+2 2
+3 3
+  4
+  0
+  1
+  2
+  3
+  4
+  0
+
+For (i = 0; i < Fils; i++){
+    \n
+    For(J   = 0; J < Cols; i++){
+        M[i][j]= Rand() % 100
+        printf("%i",\t , M[i][j])
+}
+}
+
+Cuando cargo una matríz NO la muestro
+
+*/                                     
