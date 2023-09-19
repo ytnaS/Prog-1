410da91f60e0451a5a391c14aa8b0da882551e1b
ytnaS <127530748+ytnaS@users.noreply.github.com>
2023-09-19 20:16:49 -0300

    Update Clase 12-9 .C

--- Clase 12-9 .C
+++ Clase 12-9 .C
@@ -1,31 +1,74 @@
+
 /*
-Una matriz es un vectr con dos indices  0-1-2-3-4-5-6
-                                       0
-                                       1
-                                       2
-                                       3
+mostrar(*M, CEU)
+recibe la matriz y Cantidad de elementos utiles y la recorre con 2 ciclos anidados y
+y la muestra elemento x elemento 
+La matríz va por referencia y CEU va por valor
 
-i j
-0 0
-1 1
-2 2
-3 3
-  4
-  0
-  1
-  2
-  3
-  4
-  0
+CARGAR_AZAR(*M, CEU)
+recorre la matriz con 2 ciclos anidados y carga cada elemento usando la función random
+para generar los elementos 
 
-For (i = 0; i < Fils; i++){
-    \n
-    For(J   = 0; J < Cols; i++){
-        M[i][j]= Rand() % 100
-        printf("%i",\t , M[i][j])
+Intercambia()
+recibe la matriz, la fila1 y la columna1
+guarda el valor de fila1 y la columna1 en una variable temporal 
+y le asigna el valor de fila2 y columna2 a la fila1 y la columna1
+y luego asigna el valor de fila 1 columna 1 a fila2 y columna2
+
+Main()
+define la matriz(tamaño), CEU
+llama a cargar_azar()
+llama a mostrar()
+realiza 2 ciclos anidados (I;J) y en cada vuelta cada ciclo llamar a intercambia
+pasandole fila y columna porque para el otro elemento es columna fila y llamar a mostrar 
+en cada vuelta
+*/
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+int mostrar(int M[][20], int fila, int columna){
+    int i, j;
+    for(i=0; i<fila; i++){
+        printf("\n");
+        for(j = 0; j < columna; j++){
+            printf("%i\t", M[i][j]);
+        }
+    }
 }
+ int cargar_azar(int M[][20], int fila, int columna){
+    int i, j;
+    for(i=0; i<fila; i++){
+        printf("\n");
+        for(j = 0; j < columna; j++){
+            M[i][j]= rand()% 50;
+        }
+    }
+ }
+
+int intercambia(int M[][20], int fila, int columna){ 
+    int auxiliar = M[fila][columna] ;
+    M[fila][columna] = M[columna][fila] ;
+    M[columna][fila] = auxiliar;
 }
 
-Cuando cargo una matríz NO la muestro
+int main(){
+    int M[20][20];
+    int CEUfila = 3;
+    int CEUcolumna = 3;
+    mostrar(M, CEUfila, CEUcolumna);
+    cargar_azar(M, CEUfila, CEUcolumna);
+    mostrar(M, CEUfila, CEUcolumna);
+    
 
-*/                                     
+
+
+}
+
+
+
+ /*
+ Escribir un script que verifique que una matriz cuadrada es una matriz diagonal dominante
+ Una matriz diagonal dominante es aquella en que cada valor absoluto de un elemento de 
+ la diagonal principal es mayor que la suma de los valores absolutos del resto de 
+ los elementos de su fila mas la suma de los valores absolutos del resto de os elementos
+ de su columna*/
